28cd9fca344c2ffc8bde14829adfb9315f15091a
Dayrat <manukyan_artur_1994@bk.ru>
2024-10-22 02:08:15 +0300

    Добавьте файлы проекта.

---
+++ UsersClasses/InfoGMail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WFA_SendingEmail.UsersСlasses;
+
+namespace WindowsFormsApp1.UsersClasses
+{
+    public class InfoGMail : InfoEmail
+    {
+        public InfoGMail(StringPair emailAdressTo, string subject, string body) : 
+            base (emailAdressTo, subject, body)
+        {
+            SmtpClientAdress = "smtp.gmail.com";
+            EmailAdressFrom = new StringPair("artgamer488@gmail.com", "Манукян Артур, Остапчук Антон, Королев Никита");
+            EmailPassword = "sfru nnze xwxa vwjz";
+            Port = 587;
+        }
+    }
+}
---
+++ UsersClasses/InfoMailRu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WFA_SendingEmail.UsersСlasses;
+
+namespace WindowsFormsApp1.UsersClasses
+{
+    public class InfoMailRu : InfoEmail
+    {
+        public InfoMailRu(StringPair emailAdressTo, string subject, string body) :
+            base(emailAdressTo, subject, body)
+        {
+            SmtpClientAdress = "smtp.mail.ru";
+            EmailAdressFrom = new StringPair("manukyan_artur_1994@bk.ru", "Манукян Артур, Остапчук Антон, Королев Никита");
+            EmailPassword = "imMiQNjW41DNajQ0QYbc";
+            Port = -1;
+        }
+    }
+}
